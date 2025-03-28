07637b4377984297ffe5d952a429b2003cdad52b
juansara <juan.sara@utec.edu.pe>
2025-03-27 21:12:56 -0500

    create tables

--- Lab1/database/models.py
+++ Lab1/database/models.py
@@ -1,7 +1,12 @@
-def create_sales_table(conn):
+from connection import get_connection
+
+def create_tables():
+    conn = get_connection()
     cur = conn.cursor()
+    
+
     cur.execute("""
-        CREATE TABLE IF NOT EXISTS sales (
+        CREATE TABLE IF NOT EXISTS productos (
             id SERIAL PRIMARY KEY,
             product_name TEXT NOT NULL,
             quantity INT NOT NULL,
@@ -9,5 +14,27 @@ def create_sales_table(conn):
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
     """)
+    
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            password TEXT NOT NULL,
+            saldo FLOAT NOT NULL,
+            monedero_ahorro FLOAT NOT NULL,
+        );
+    """)
+
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS tiendas (
+            id SERIAL PRYMARY KEY,
+            nombre TEXT NOT NULL,
+            direccion TEXT NOT NULL,
+        );
+    """)
+
     conn.commit()
     cur.close()
+    conn.close()
