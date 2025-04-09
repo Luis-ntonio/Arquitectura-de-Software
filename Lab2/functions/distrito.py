465f8d332f5db459f1b736755eb8374e50ceea13
juansara <juan.sara@utec.edu.pe>
2025-04-09 01:02:35 -0500

    Update Distrito

--- Lab2/functions/distrito.py
+++ Lab2/functions/distrito.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, HTTPException, status, Query, Body
 from typing import Dict, Any, List, Optional
-from database import distrito_db
+from database import distrito_db, Distrito
 from models import Distrito
 
 router = APIRouter()
 
 router.get("/distritos")
-def get_distritos() -> List[Dict[str, Any]]:
+def get_distritos() -> List[Dict[str, Distrito]]:
     return list(distrito_db.values())
